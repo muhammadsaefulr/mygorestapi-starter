a55e963b46937006dbe5489d72579f935d52c547
muhammadsaefulr <saepull.id@gmail.com>
2025-07-06 19:34:39 +0700

    fix: temporary disable for foreign key to detail

--- internal/domain/model/history_model.go
+++ internal/domain/model/history_model.go
@@ -7,12 +7,12 @@ import (
 )
 
 type History struct {
-	ID           uint      `gorm:"primaryKey" json:"id"`
-	UserId       uuid.UUID `gorm:"not null" json:"user_id"`
-	MovieId      string    `gorm:"not null" json:"movie_id"`
-	MovieEpsId   string    `gorm:"uniqueIndex:idx_history_movie_eps_id" json:"movie_eps_id"`
-	PlaybackTime int       `gorm:"not null" json:"playback_time"`
-	CreatedAt    time.Time `gorm:"autoCreateTime" json:"created_at"`
-	DetailMovie  AnimeDetail
+	ID           uint        `gorm:"primaryKey" json:"id"`
+	UserId       uuid.UUID   `gorm:"not null" json:"user_id"`
+	MovieId      string      `gorm:"not null" json:"movie_id"`
+	MovieEpsId   string      `gorm:"uniqueIndex:idx_history_movie_eps_id" json:"movie_eps_id"`
+	PlaybackTime int         `gorm:"not null" json:"playback_time"`
+	CreatedAt    time.Time   `gorm:"autoCreateTime" json:"created_at"`
+	DetailMovie  AnimeDetail `gorm:"-"`
 	UpdatedAt    time.Time
 }
